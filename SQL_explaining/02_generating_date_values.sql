19bc36023d8c8522bd00ec50c3993d81d7eb2698
Aleksandr Morshinin <t89043804427@gmail.com>
2024-01-14 14:29:53 +0300

    Update 02_generating_date_values.sql

--- SQL_explaining/02_generating_date_values.sql
+++ SQL_explaining/02_generating_date_values.sql
@@ -18,3 +18,30 @@ INSERT INTO calendar_test (order_date)
 VALUES (
 DATE(generate_series(DATE(NOW()) - INTERVAL '10 year', DATE(NOW()), INTERVAL '1 day'))
 );
+
+
+-- Вариант 2. Заливка диапазона в 1 год от min и max значений в таблице фактов
+-- Пока не разобрался. Например. была ошибка "функции, возвращающие множества, нельзя применять в конструкции VALUES"
+
+select
+	all_dates.*
+ FROM
+(
+select
+	generate_series(
+	'2000-01-01'::date,
+	DATE(NOW()),
+--		DATE(NOW()) - INTERVAL '20 year',
+--		DATE(NOW()),
+		INTERVAL '1 day'
+	) date_column
+) all_dates
+join
+(
+	select
+		min(order_date)::date - INTERVAL '1 year' as min_date,
+		max(order_date)::date + INTERVAL '1 year' as max_date
+	from orders 
+) min_max
+on all_dates.date_column >= min_max.min_date and all_dates.date_column <= min_max.max_date
+;
