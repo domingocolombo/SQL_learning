eef3e851560b696da06063f9a781f0dec7996049
Aleksandr Morshinin <t89043804427@gmail.com>
2024-01-14 14:48:34 +0300

    Update 02_generating_date_values.sql

--- SQL_explaining/02_generating_date_values.sql
+++ SQL_explaining/02_generating_date_values.sql
@@ -45,3 +45,25 @@ join
 ) min_max
 on all_dates.date_column >= min_max.min_date and all_dates.date_column <= min_max.max_date
 ;
+
+
+
+-- Посмотреть ещё на такой код потом:
+
+select 
+to_char(date,'yyyymmdd')::int as date_id,  
+       extract('year' from date)::int as year,
+       extract('quarter' from date)::int as quarter,
+       extract('month' from date)::int as month,
+       extract('week' from date)::int as week,
+       date::date,
+       to_char(date, 'dy') as week_day,
+       extract('day' from
+               (date + interval '2 month - 1 day')
+              ) = 29
+       as leap
+  from generate_series(date '2000-01-01',
+                       date '2030-01-01',
+                       interval '1 day')
+       as t(date);
+--checking
